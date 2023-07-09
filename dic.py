0d76a8dfa12ac7cc0e9f750488f25f415201a7a8
forin <foringsarkar@gmail.com>
2023-07-09 19:53:48 +0000

    rwerwer

---
+++ dic.py
@@ -0,0 +1,18 @@
+Capitals = {"Usa": "Washington DC",
+            "China": "Beijing",
+            "Russia": "Moscow",
+            "India": "New Delhi"}
+
+print(Capitals)
+
+Capitals.update({"America": "Washington DC"})
+print(Capitals)
+
+Capitals.pop('China')
+print(Capitals)
+
+
+print(Capitals.keys())
+
+
+print(Capitals.values())
